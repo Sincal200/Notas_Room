d9ebb20ea950ef6624fb318ceca32d776d3701b3
Sincal <sincaljulio200@gmail.com>
2022-10-31 13:32:17 -0600

    Añadiendo Base de Datos

---
+++ app/src/main/java/com/example/notas_android/NotaRoomDataBase.java
@@ -0,0 +1,27 @@
+package com.example.notas_android;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {NotaEntity.class}, version = 1)
+public abstract class NotaRoomDataBase extends RoomDatabase {
+    public abstract NotaDao notaDao();
+    private static volatile NotaRoomDataBase INSTANCE;
+
+    public static NotaRoomDataBase getDataBase(final Context context){
+        if(INSTANCE == null){
+            synchronized (NotaRoomDataBase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NotaRoomDataBase.class, "notas_database")
+                            .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+}
