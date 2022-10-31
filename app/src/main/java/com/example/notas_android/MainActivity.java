5715119513a7702ff76836a69ede9f326d85b798
Sincal <sincaljulio200@gmail.com>
2022-10-31 08:42:08 -0600

    Creación de Navigation View

--- app/src/main/java/com/example/notas_android/MainActivity.java
+++ app/src/main/java/com/example/notas_android/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.notas_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        //Definir el evento click sobre el botón de Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,NotasActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
